ef3082de3a9f779bccc635b0833e4ce42c817174
jvorvick <vorvickjesse@gmail.com>
2024-02-27 16:31:31 -0800

    test_ex3

--- tests/test_ex1.py
+++ tests/test_ex1.py
@@ -1,28 +1,30 @@
-import pytest
+# import pytest
 
-'''
-by default
-function - run once per test
-class - run once per class of tests
-module - run once per module
-session - run once per session
-'''
+# '''
+# by default
+# function - run once per test
+# class - run once per class of tests
+# module - run once per module
+# session - run once per session
+# '''
 
-@pytest.fixture(scope="session")
-def fixture_1():
-    print('run-fixture-1')
-    return 1
+# @pytest.fixture(scope="session")
+# def fixture_1():
+#     print('run-fixture-1')
+#     return 1
 
-def test_example1(fixture_1):
-    print('run-example-1')
-    num = fixture_1
-    assert num == 1
+# def test_example1(fixture_1):
+#     print('run-example-1')
+#     num = fixture_1
+#     assert num == 1
 
-def test_example2(fixture_1):
-    print('run-example-2')
-    num = fixture_1
-    assert num == 1
+# def test_example2(fixture_1):
+#     print('run-example-2')
+#     num = fixture_1
+#     assert num == 1
 
+# OLD
+    
 # @pytest.mark.slow
 # def test_example():
 #     print("test1")
--- tests/test_ex2.py
+++ tests/test_ex2.py
@@ -1,11 +1,11 @@
-import pytest
+# import pytest
 
-@pytest.fixture
-def yield_fixture():
-    print('Start Test Phase')
-    yield 6
-    print('End Test Phase')
+# @pytest.fixture
+# def yield_fixture():
+#     print('Start Test Phase')
+#     yield 6
+#     print('End Test Phase')
 
-def test_example(yield_fixture):
-    print('run-example-1')
-    assert yield_fixture == 6
+# def test_example(yield_fixture):
+#     print('run-example-1')
+#     assert yield_fixture == 6
