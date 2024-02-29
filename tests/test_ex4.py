a78ab2dc59a7d747452ca63cc75f47298ab67ff6
jvorvick <vorvickjesse@gmail.com>
2024-02-28 17:07:06 -0800

    added models

--- tests/test_ex4.py
+++ tests/test_ex4.py
@@ -1,4 +1,7 @@
- 
+import pytest
+
+from django.contrib.auth.models import User
+
 # @pytest.fixture()
 # def user_1(db):
 #     return User.objects.create_user('test-user')
@@ -24,6 +27,23 @@
 #     print('check-user2')
 #     assert user_1.username == 'test-user'
 
-def test_new_user(new_user2):
-    print(new_user2.is_staff)
-    assert new_user2.is_staff
+# def test_new_user(new_user2):
+#     print(new_user2.is_staff)
+#     assert new_user2.is_staff
+
+# @pytest.mark.django_db
+# def test_new_user(user_factory):
+#     user = user_factory.build()
+#     count = User.objects.all().count()
+#     print(count)
+#     print(user.username)
+#     assert True
+
+# def test_new_user(new_user1):
+#     print(new_user1.username)
+#     assert True
+
+def test_product(product_factory):
+    product = product_factory.build()
+    print(product.description)
+    assert True
